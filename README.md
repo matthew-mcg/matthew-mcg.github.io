bc4376ba79bd91b8e99d90a3646d16aca3c4102b
Matthew M <mjmcgowan7@gmail.com>
2020-06-03 11:36:56 -0400

    wip 2020 site

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MisterMista.github.io
+
+TODO
+ - add navigation bar at top (with drop shadow to look nice)
+ - better color theme
+ - boxes/dividers for sections
+ - _responsive design_
--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-/*8window.onload=*/function color(){
-    var bg = document.getElementById("color");
-    bg.style.backgroundColor="rgba("+rand()+","+rand()+","+rand()+",.15)";
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+  
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('#splash').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
 }
 
-function rand(){//Gets random int between 0 and 255
-    return Math.floor(Math.random() * 255);
-}
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+moveBackground();
